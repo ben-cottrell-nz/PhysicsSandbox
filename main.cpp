df3daf681f420a612c58dff3020194cc08c367dc
Ben Cottrell <bencot@windowslive.com>
2025-03-20 19:50:15 +1300

    Experimenting with Vulkan and Skia

--- main.cpp
+++ main.cpp
@@ -1,21 +1,28 @@
 #include "mainwindow.h"
 #include <QApplication>
-#include <QOpenGLContext>
+#include <QVulkanInstance>
+#include "physworldview_vulkan.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    QSurfaceFormat fmt;
-    fmt.setSamples(24);
-    if (QOpenGLContext::openGLModuleType() == QOpenGLContext::LibGL) {
-        fmt.setVersion(3,3);
-        fmt.setProfile(QSurfaceFormat::CoreProfile);
-    } else {
-        fmt.setVersion(3,0);
+
+    QVulkanInstance inst;
+    inst.setLayers({"VK_LAYER_KHRONOS_validation"});
+    if (!inst.create()) {
+        qFatal("Failed to create the Vulkan instance: %d", inst.errorCode());
     }
-    QSurfaceFormat::setDefaultFormat(fmt);
-    MainWindow w;
-    w.resize(1152,968);
+    qDebug("Started app");
+    std::shared_ptr<QVulkanInstance> pVkInst(&inst);
+    PhysWorldViewVulkan vkw;
+    vkw.setVulkanInstance(pVkInst.get());
+    QMainWindow w;
+    // freed by QMainWindow
+    QWidget* wrapper(QWidget::createWindowContainer(&vkw, &w));
+    w.setCentralWidget(wrapper);
+    w.resize(1024,768);
     w.show();
-    return a.exec();
+    auto r = a.exec();
+    qDebug() << "PhysWorldViewVulkan_refcnt: " << PhysWorldViewVulkan_refcnt;
+    return r;
 }
